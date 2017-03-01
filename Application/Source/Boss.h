74258cc49c930d3b2bcb853949124b2b669c9fcf
Psidra <xdusk37@gmail.com>
2017-03-01 14:30:16 +0800

    Animations added, boss shoots projectiles, new bouncy projectile. :D

--- Application/Source/Boss.h
+++ Application/Source/Boss.h
@@ -60,7 +60,9 @@ private:
 	double bufferTime_iframe;
 	double bufferTime_attackchoice;
 	double bufferTime_projduration;
+	double bufferTime_projcd;
 	unsigned int attackchoice;
+	unsigned int projtype;
 	float direction_p_a, direction_p_b;
 	float offset;
 	int flip_a, flip_b;
